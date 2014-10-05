f45025d7681a2cfa46264fdc180c3e67ba0ab59b
randomguy64 <64.randomguy@gmail.com>
2014-10-05 00:22:39 -0400

    moved things to server, classified map, fixed commander

---
+++ server/game_map.py
@@ -0,0 +1,12 @@
+class GameMap:
+
+    def __init__(self, rows, cols, map):  # map is a string of 0s and 1s, 
+        # 0 meaning no wall, 1 meaning wall, starting in the 
+        # top left and going across first, then down.
+        # The coordinate of a wall is the top left corner's cartesian coordinate.
+        self.walls = []
+        for r in xrange(rows):
+            walls[r] = []
+            for c in xrange(cols):
+                walls[r][c] = int(map[r * cols + c])
+
